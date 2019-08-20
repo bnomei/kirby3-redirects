01064278874c7e4a9b26f2e1953326d750887d83
Bruno Meilick <b@bnomei.com>
2019-08-20 11:48:13 +0200

    routes test
    Signed-off-by: Bruno Meilick <b@bnomei.com>

--- tests/IndexTest.php
+++ tests/IndexTest.php
@@ -13,21 +13,21 @@ class IndexTest extends TestCase
     public function testRedirectCodesRoute()
     {
         $response = kirby()->render('/plugin-redirects/codes');
-        $this->assertIsInt($response->code(), 200);
+        $this->assertTrue($response->code() === 200);
         $this->assertStringStartsWith('application/json', $response->type());
     }
 
     public function testFindsHomePage()
     {
         $response = kirby()->render('/');
-        $this->assertIsInt($response->code(), 200);
+        $this->assertTrue($response->code() === 200);
         $this->assertStringContainsString('Home', $response->body());
     }
 
     public function testFindsTestPage()
     {
         $response = kirby()->render('/projects/ahmic');
-        $this->assertIsInt($response->code(), 200);
+        $this->assertTrue($response->code() === 200);
         $this->assertStringContainsString('Ahmic', $response->body());
     }
 }
